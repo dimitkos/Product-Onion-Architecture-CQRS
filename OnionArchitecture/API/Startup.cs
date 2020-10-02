9b050f35372d1e4c5e3942aa977220d66fd9b21a
dimitkos <User1@DESKTOP-D6C1HHM>
2020-10-02 17:13:30 +0300

    Add context in infrastucure layer, also add in start up the services from the layers

--- OnionArchitecture/API/Startup.cs
+++ OnionArchitecture/API/Startup.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Application;
+using Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace API
@@ -27,7 +23,8 @@ namespace API
         {
             services.AddControllers();
 
-            //services.AddApplication();
+            services.AddApplication();
+            services.Infrastructure(Configuration);
 
             services.AddSwaggerGen(c =>
             {
---
+++ OnionArchitecture/Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Context
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await base.SaveChangesAsync();
+        }
+    }
+}
