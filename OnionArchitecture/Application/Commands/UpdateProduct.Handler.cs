cfdfbc228b0ce3c21274e5fc5cd7e520025bca2a
dimitkos <User1@DESKTOP-D6C1HHM>
2020-10-02 16:27:12 +0300

    Implement update product in application layer

---
+++ OnionArchitecture/Application/Commands/UpdateProduct.Handler.cs
@@ -0,0 +1,31 @@
+﻿using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Commands
+{
+    public class UpdateProductHandler : IRequestHandler<UpdateProduct, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public UpdateProductHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(UpdateProduct request, CancellationToken cancellationToken)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            if (product == null)
+                return default;
+
+            _context.Products.Update(product);
+            await _context.SaveChanges();
+
+            return product.Id;
+        }
+    }
+}
--- OnionArchitecture/Application/Commands/UpdateProduct.cs
+++ OnionArchitecture/Application/Commands/UpdateProduct.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using MediatR;
 
 namespace Application.Commands
 {
-    class UpdateProduct
+    public class UpdateProduct : IRequest<int>
     {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Barcode { get; private set; }
+        public string Description { get; private set; }
+        public decimal Rate { get; private set; }
+
+        public UpdateProduct(int id, string name, string barcode, string description, decimal rate)
+        {
+            Id = id;
+            Name = name;
+            Barcode = barcode;
+            Description = description;
+            Rate = rate;
+        }
     }
 }
