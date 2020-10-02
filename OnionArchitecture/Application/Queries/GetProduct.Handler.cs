0a8e0f3effc2c5e3397c7d1a2e20006bb00c1ce2
dimitkos <User1@DESKTOP-D6C1HHM>
2020-10-02 16:13:59 +0300

    Implement get product in application layer

---
+++ OnionArchitecture/Application/Queries/GetProduct.Handler.cs
@@ -0,0 +1,29 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Queries
+{
+    public class GetProductHandler : IRequestHandler<GetProductQuery, Product>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetProductHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> Handle(GetProductQuery request, CancellationToken cancellationToken)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(x => x.Id == request.ProductId);
+
+            if (product == null)
+                return null;
+
+            return product;
+        }
+    }
+}
---
+++ OnionArchitecture/Application/Queries/GetProduct.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using MediatR;
+
+namespace Application.Queries
+{
+    public class GetProductQuery : IRequest<Product>
+    {
+        public int ProductId { get; private set; }
+
+        public GetProductQuery(int productId)
+        {
+            ProductId = productId;
+        }
+    }
+}
