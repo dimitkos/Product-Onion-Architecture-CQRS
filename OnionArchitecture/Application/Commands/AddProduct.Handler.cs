b96d92bfca76905f9df42d6a1d32230a42371e41
dimitkos <User1@DESKTOP-D6C1HHM>
2020-10-02 16:14:34 +0300

    Implement add product in application layer

---
+++ OnionArchitecture/Application/Commands/AddProduct.Handler.cs
@@ -0,0 +1,34 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Commands
+{
+    public class AddProductHandler : IRequestHandler<AddProduct, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public AddProductHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(AddProduct request, CancellationToken cancellationToken)
+        {
+            var product = new Product
+            {
+                Name = request.Name,
+                Barcode = request.Barcode,
+                Description = request.Description,
+                Rate = request.Rate
+            };
+
+            var result = _context.Products.Add(product);
+            await _context.SaveChanges();
+
+            return product.Id;
+        }
+    }
+}
--- OnionArchitecture/Application/Commands/AddProduct.cs
+++ OnionArchitecture/Application/Commands/AddProduct.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using MediatR;
 
 namespace Application.Commands
 {
-    class AddProduct
+    public class AddProduct : IRequest<int>
     {
+        public string Name { get; private set; }
+        public string Barcode { get; private set; }
+        public string Description { get; private set; }
+        public decimal Rate { get; private set; }
+
+        public AddProduct(string name, string barcode, string description, decimal rate)
+        {
+            Name = name;
+            Barcode = barcode;
+            Description = description;
+            Rate = rate;
+        }
     }
 }
