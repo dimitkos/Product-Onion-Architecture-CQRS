604ad28e306bf98f60ff5e06c1751a8cec131fd1
dimitkos <User1@DESKTOP-D6C1HHM>
2020-10-02 14:40:05 +0300

    Add Entity and base entity

---
+++ OnionArchitecture/Domain/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Barcode { get; set; }
+        public string Description { get; set; }
+        public decimal Rate { get; set; }
+    }
+}
