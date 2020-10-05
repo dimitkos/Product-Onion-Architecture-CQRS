099ea94193aaaf7c3fdaf77adfcbb16a7183ba98
dimitkos <User1@DESKTOP-D6C1HHM>
2020-10-05 10:33:52 +0300

    Implement product controller,also fix a bug in update method

---
+++ OnionArchitecture/API/Controllers/ProductController.cs
@@ -0,0 +1,95 @@
+﻿using Application.Commands;
+using Application.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Gets all Products.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _mediator.Send(new GetAllProductsQuery());
+
+            if (products == null)
+                return BadRequest();
+
+            return Ok(products);
+        }
+
+        /// <summary>
+        /// Gets Product Entity by Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _mediator.Send(new GetProductQuery(productId: id));
+
+            if (product == null)
+                return BadRequest();
+
+            return Ok(product);
+        }
+
+        /// <summary>
+        /// Creates a New Product.
+        /// </summary>
+        /// <param name="addProduct"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] AddProduct addProduct)
+        {
+            var productId = await _mediator.Send(addProduct);
+
+            return Ok(productId);
+        }
+
+        /// <summary>
+        /// Updates the Product Entity based on Id.   
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="updataProduct"></param>
+        /// <returns></returns>
+        [HttpPut("[action]")]
+        public async Task<IActionResult> Update(int id, [FromBody] UpdateProduct updataProduct)
+        {
+            if (id != updataProduct.Id)
+                return BadRequest();
+
+            var productId = await _mediator.Send(updataProduct);
+
+            return Ok(productId);
+        }
+
+        /// <summary>
+        /// Deletes Product Entity based on Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var productId = await _mediator.Send(new DeleteProduct(productId: id));
+
+            return Ok(productId);
+        }
+    }
+}
